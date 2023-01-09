e53b29b67fa79ce9fd7e0140feb56ca94c2c2b81
Test Engineer <oyhammad@outlook.com>
2023-01-09 19:55:35 +0200

    Add config

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,10 @@ export default defineConfig({
     viewportWidth: 1366,
     viewportHeight: 768,
     baseUrl: 'https://test.egypt.mylerz.com/login',
-
+    retries: {
+      "runMode": 2,
+      "openMode": 2
+    },
     setupNodeEvents(on, config)
     {
       // implement node event listeners here
